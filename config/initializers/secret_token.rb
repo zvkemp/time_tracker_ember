f6d50340a4b050c03c651023fdaffdd9fa2cc494
zvkemp <zvkemp@gmail.com>
2014-06-24 08:12:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeTrackerEmber::Application.config.secret_key_base = 'a6ed04a4ef54a49f93acedcd8930ea0a96ad6bba67ff6b777fb5ecd688e564325939f1ae443424bf7624e4513e00dbbddc03084cdb151e7027e6146cf824af62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeTrackerEmber::Application.config.session_store :cookie_store, key: '_time_tracker_ember_session'
